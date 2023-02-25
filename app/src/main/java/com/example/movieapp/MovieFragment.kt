f9b2bc230488156116929e260fbc90fe57085dcf
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-02-25 11:47:55 -0300

    Agregando ViewBinding

--- app/src/main/java/com/example/movieapp/MovieFragment.kt
+++ app/src/main/java/com/example/movieapp/MovieFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.movieapp.databinding.FragmentMovieBinding
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
+    private lateinit var binding: FragmentMovieBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
     }
 
 }
