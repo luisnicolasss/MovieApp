6603f7851f93461b182b48af367a6e4e83daa947
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-03-28 15:16:42 -0300

    Capa de repositorio

---
+++ app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.application
+
+object AppConstants {
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "1ab451fe3aeed0a2273570ff79a7e824"
+}
