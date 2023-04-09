8ac295103ebd0c1e89a1b09a7fbb79272dd9fb21
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-04-08 23:27:53 -0300

    Entity

--- app/src/main/java/com/example/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/model/Movie.kt
@@ -1,5 +1,9 @@
 package com.example.movieapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 data class Movie(
     val id: Int = -1,
     val adult: Boolean = false,
@@ -18,3 +22,34 @@ data class Movie(
 )
 
 data class MovieList(val results: List<Movie> = listOf())
+
+//Room
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo("adult")
+    val adult: Boolean = false,
+    @ColumnInfo("backdrop_path")
+    val backdrop_path: String = "",
+    @ColumnInfo("original_title")
+    val original_title: String = "",
+    @ColumnInfo("original_language")
+    val original_language: String = "",
+    @ColumnInfo("overview")
+    val overview: String = "",
+    @ColumnInfo("popularity")
+    val popularity: Double = -1.0,
+    @ColumnInfo("poster_path")
+    val poster_path: String = "",
+    @ColumnInfo("release_date")
+    val release_date: String = "",
+    @ColumnInfo("title")
+    val title: String = "",
+    @ColumnInfo("video")
+    val video: Boolean = false,
+    @ColumnInfo("vote_average")
+    val vote_average: Double = -1.0,
+    @ColumnInfo("vote_count")
+    val vote_count: Int = -1
+)
